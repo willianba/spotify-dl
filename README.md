5762fdfacc3e543d011ef57c4a245e69bd68d110
Willian Alves <wba.alves@gmail.com>
2017-09-29 20:27:28 -0300

    Added method to download playlists; removed changelog; changed README.

--- README.md
+++ README.md
@@ -3,14 +3,16 @@
 [![GitHub license](https://img.shields.io/badge/author-naper-blue.svg)](https://raw.githubusercontent.com/Facetracker-project/facetracker-core/master/COPYING)
 [![GitHub license](https://img.shields.io/badge/version-0.0.2-orange.svg)](https://raw.githubusercontent.com/Facetracker-project/facetracker-core/master/COPYING)
 # spotify-dl
-a script that allows you to download spotify songs or playlists , written in python
+A script that allows you to download spotify songs or playlists, written in python
 
 This README would normally document whatever steps are necessary to get spotify-dl up and running.
+
+This repo was forked from [Hamza Bourrahim](https://github.com/invicnaper) and modified to download either tracks and playlists.
 
 ### What is this repository for? ###
 
 * spotify-dl allows you to download spotify songs or playlist
-* Version 0.0.2
+* Version 0.0.3
 * This repo contains spotify-dl source code
 
 ### Screen ###
@@ -18,57 +20,69 @@ This README would normally document whatever steps are necessary to get spotify-
 ![alt text](http://nsa37.casimages.com/img/2016/02/13/160213111903934479.png "spotfy-dl screen")
 
 # How to Install ?
-to use spotify-dl , you need to install thoses packages :
+To use spotify-dl, you need to install thoses packages :
   * bs4
   * youtube-dl
   
 # MAC OSx
-you can use brew to install youtube-dl :
+You can use brew to install youtube-dl:
   
     $ brew install youtube-dl
     
-and pip to install bs4
+And pip to install bs4:
   
     $ pip install beautifulsoup4
     
 # Linux (debian)
-use apt-get install to install youtube-dl
+Use apt-get install to install youtube-dl:
 
     $ sudo apt-get install youtube-dl
     
-and pip to install bs4
+And pip to install bs4:
   
     $ pip install beautifulsoup4
-    
-# How to use ?
-you can either use your spotify account or downloading single track or playlist by providing an ID , ex:
-
-    $ ./spotify-dl --track {spotify_song_id} --dl youtube
-    
-this will download the track and save it as mp3 format
-
-you can get the song ID by getting the spotify URI of the song
-
-{spotify_song_id_ex} : 28Ct4qwkQXY2W5yyNCLuVI
 
 # Spotify API
-The new version of the spotify api require an access_token for requests, you can check out https://developer.spotify.com/migration-guide-for-unauthenticated-web-api-calls/.
+The new version of the spotify api require an access_token for requests you can check out https://developer.spotify.com/migration-guide-for-unauthenticated-web-api-calls/.
 
-the new patch of spotify-dl have a new argument called:  --access_token , so the new usage of spotify-dl would be:
+The new patch of spotify-dl have a new argument called: --access_token, so the new usage of spotify-dl would be:
   
     $ ./spotify-dl --track {spotify_song_id} --dl youtube --access_token <your_access_token>
     
-you can get the access token from the url generated while executing : 
+You can get the access token from the url generated while executing: 
 
     $ ./spotify-dl --gen_url 
     
-you also have to create an application on https://developer.spotify.com/
+You also have to create an application on https://developer.spotify.com/.
 
-change:
+Or you can get one on https://developer.spotify.com/web-api/console/get-playlist-tracks/, just click 'generate oauth token' and copy it.
 
-    CLIENT_ID=""
-    CALL_BACK_URL=""
+Then, change:
+
+    CLIENT_ID="spotify-app-id"
+    CALL_BACK_URL="url"
+    DEVELOPER_KEY="youtube-api-key"
+
+# How to use ?
+You can either use your spotify account or downloading single track or playlist by providing an ID. Ex.:
+
+    $ ./spotify-dl --track {spotify_song_id} --dl youtube
+    
+This will download the track and save it as mp3 format
+
+You can get the song ID by getting the spotify URI of the song
+
+{spotify_song_id_ex} : 28Ct4qwkQXY2W5yyNCLuVI
+
+You can download an entire playlist by providing the playlist's user and link:
+
+    $ ./spotify-dl --playlist {spotify_playlist_link} --user {playlist_user} --dl youtube --access_token <your_access_token>
+
+If you want to set an output folder, you can do it providing the folder name between "". Ex.:
+
+    $ ./spotify-dl --playlist {spotify_playlist_link} --user {playlist_user} --dl youtube --access_token <your_access_token> --folder {"folder name"}
+
 
 ### Contributors ###
-
 * Hamza Bourrahim
+* Willian Alves
--- spotify-dl.py
+++ spotify-dl.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 
-#from googleapiclient.discovery import build
-#from apiclient.errors import HttpError
-#from oauth2client.tools import argparser
-
+import os
+import sys
+import json
 import urllib
 import urllib2
-from bs4 import BeautifulSoup
 import argparse
-#import spotify
-import json
-from StringIO import StringIO
-import subprocess
 import traceback
+import subprocess
+from bs4 import BeautifulSoup
+from StringIO import StringIO
 
 RED     = "\033[31m"
 GREEN   = "\033[32m"
@@ -27,14 +24,14 @@ OK      =  GREEN + "[+] " + DEFAULT
 #=======================
 #   Spotify application
 #=======================
-CLIENT_ID=""
-CALL_BACK_URL=""
+CLIENT_ID="REPLACE_HERE"
+CALL_BACK_URL="REPLACE_HERE"
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "REPLACE_ME"
+DEVELOPER_KEY = "REPLACE_HERE"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
@@ -83,12 +80,9 @@ def searchYoutube(trackname):
 
 def getTrackName(id, access_token):
     """ get the spotify track name from id """
-    print ACTION + " getting track name"
+    print ACTION + "getting track name"
     proc = subprocess.Popen('curl -sS -X GET "https://api.spotify.com/v1/tracks/'+ id +'?market=ES" -H "Authorization: Bearer '+ access_token +'"', shell=True, stdout=subprocess.PIPE)
     tmp = proc.stdout.read()
-    #convert from json to string
-    #io = StringIO()
-    #json.dump(tmp, io)
     data = json.loads(tmp)
     if 'error' in data:
         print ERROR + "can't found song name"
@@ -98,9 +92,26 @@ def getTrackName(id, access_token):
         print OK + "name is " + data["name"]
         return data["name"]
 
+def getPlaylistSongs(user, playlist, access_token):
+    """ get the spotify tracks names from playlist link """
+    print ACTION + "getting tracks names"
+    proc = subprocess.Popen('curl -sS -X GET "https://api.spotify.com/v1/users/' + user + '/playlists/' + playlist + '/tracks?market=ES&fields=items(track)" -H "Authorization: Bearer '+ access_token +'"', shell=True, stdout=subprocess.PIPE)
+    tmp = proc.stdout.read()
+    data = json.loads(tmp)
+    names = []
+    for item in data['items']:
+        if 'error' in data:
+            print ERROR + "can't found song name"
+            print ERROR + data['error']['message']
+            return None
+        else:
+            name = str(item['track']['name'])
+            names.append(name)
+    return names
+
 def genUrl():
     """ gen url for getting access token """
-    print ACTION + " generating url for access token"
+    print ACTION + "generating url for access token"
     print OK +  "https://accounts.spotify.com/authorize?client_id="+ CLIENT_ID + "&response_type=token&redirect_uri=" + CALL_BACK_URL
 
 def getAccessToken():
@@ -119,45 +130,66 @@ def downloadYoutube(link):
     tmp = proc.stdout.read()
     print OK + "Song Downloaded"
 
+def rename_songs(path):
+    filenames = os.listdir(path)
+    for filename in filenames:
+        new_name = filename.split('-')[0] + "-" + filename.split('-')[1] + ".mp3"
+        os.rename(path + "/" + filename, path + "/" + new_name)
+
 def header():
-	""" header informations """
-	print RED + "@ spotify-dl.py version 0.0.1"
-	print YELLOW + "@ author : Naper"
-	print BLUE + "@ Designed for OSx/linux"
-	print "" + DEFAULT
+    """ header informations """
+    print RED + "@ spotify-dl.py version 0.0.3"
+    print YELLOW + "@ author : Naper"
+    print BLUE + "@ Designed for OSx/linux"
+    print "" + DEFAULT
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='spotify-dl allows you to download your spotify songs')
-  parser.add_argument('--verbose',
-    action='store_true',
-    help='verbose flag' )
-  parser.add_argument('--dl', nargs=1, help="set the download methode")
-  parser.add_argument('--user', nargs=1, help="set the spotify login")
-  parser.add_argument('--password', nargs=1, help="set the spotify password")
-  parser.add_argument('--traceback', action='store_true', help="enable traceback")
-  parser.add_argument('--gen_url', action='store_true', help="generate url for getting access_token")
-  parser.add_argument('--track', nargs=1, help="spotify track id")
-  parser.add_argument('--access_token', nargs=1, help="set the access_token")
-  parser.add_argument('-m', nargs=1, help="set a methode")
+    parser = argparse.ArgumentParser(description='spotify-dl allows you to download your spotify songs')
+    parser.add_argument('--verbose', action='store_true', help='verbose flag')
+    parser.add_argument('--dl', nargs=1, help="set the download methode")
+    parser.add_argument('--user', nargs=1, help="set the spotify login")
+    parser.add_argument('--password', nargs=1, help="set the spotify password")
+    parser.add_argument('--traceback', action='store_true', help="enable traceback")
+    parser.add_argument('--gen_url', action='store_true', help="generate url for getting access_token")
+    parser.add_argument('--track', nargs=1, help="spotify track id")
+    parser.add_argument('--playlist', nargs=2, help="spotify playlist's user and link")
+    parser.add_argument('--access_token', nargs=1, help="set the access_token")
+    parser.add_argument('--folder', nargs=1, help="set output folder")
+    parser.add_argument('-m', nargs=1, help="set a methode")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  try:
-      header();
-      if args.gen_url:
-          genUrl()
-      else:
-          if args.dl and args.access_token and args.dl[0] == 'youtube':
-              if args.track:
-                  #genUrl()
-                  #getAccessToken()
-                  name = getTrackName(args.track[0], args.access_token[0])
-              link = searchYoutube(name)
-              downloadYoutube(link)
-          else :
-              print ERROR + "use --help for help"
-  except Exception, err:
-    print ERROR + "An HTTP error occurred\n"
-    if args.traceback:
-    	traceback.print_exc()
+    reload(sys)  
+    sys.setdefaultencoding('utf-8')
+
+    try:
+        header();
+        if args.gen_url:
+            genUrl()
+        else:
+            if args.dl and args.access_token and args.dl[0] == 'youtube':
+                if args.track:
+                    name = getTrackName(args.track[0], args.access_token[0])
+                    link = searchYoutube(name)
+                    downloadYoutube(link)
+                elif args.playlist:
+                    names = getPlaylistSongs(args.playlist[0], args.playlist[1], args.access_token[0])
+                    for song in names:
+                        if song != None:
+                            print OK + "name is " + song
+                            link = searchYoutube(song)
+                            downloadYoutube(link)
+                if args.folder:
+                    if not os.path.isdir(args.folder[0]):
+                        create_folder = 'mkdir "'+ args.folder[0] +'"'
+                        os.system(create_folder)
+                    move = 'mv *.mp3 ' + os.getcwd() + '/' + args.folder[0]
+                    os.system(move)
+                    rename_songs(args.folder[0])
+            else:
+                print ERROR + "use --help for help"
+    except Exception, err:
+        print ERROR + "An HTTP error occurred\n"
+        if args.traceback:
+            traceback.print_exc()
